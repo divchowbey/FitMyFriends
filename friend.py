f42979092b0df8e8df6cda1a73671b79ffc9abed
emiliag345 <43304945+emiliag345@users.noreply.github.com>
2019-11-02 16:48:45 -0400

    Update friend.py

--- friend.py
+++ friend.py
@@ -4,10 +4,13 @@ from db import get_db
 
 #class Friend(UserMixin):
 class Friend():
-    def __init__(self, id_, user_id, friend_user_id):
+    def __init__(self, id_, user_id, friend_user_id, friend_name, friend_email, friend_phone):
         self.friend_id = id_
         self.user_id = user_id
         self.friend_user_id = friend_user_id
+        self.friend_name = friend_name
+        self.friend_email = friend_email
+        self.friend_phone = friend_phone
 
     @staticmethod
     def get(friend_id):
@@ -19,16 +22,16 @@ class Friend():
             return None
 
         friend = Friend(
-            id_=friend[0], user_id=friend[1], friend_user_id=friend[2]
+            id_=friend[0], user_id=friend[1], friend_user_id=friend[2], friend_name=friend[3], friend_email=friend[4], friend_phone = friend[5]
         )
         return friend
 
     @staticmethod
-    def create(id_, user_id, friend_user_id):
+    def create(id_, user_id, friend_user_id, friend_name, friend_email, friend_phone):
         db = get_db()
         db.execute(
-            "INSERT INTO friend (friend_id, user_id, friend_user_id) "
-            "VALUES (?, ?, ?)",
-            (id_, user_id, friend_user_id),
+            "INSERT INTO friend (friend_id, user_id, friend_user_id, friend_name, friend_email, friend_phone) "
+            "VALUES (?, ?, ?, ?, ?, ?)",
+            (id_, user_id, friend_user_id, friend_name, friend_email, friend_phone),
         )
         db.commit()
