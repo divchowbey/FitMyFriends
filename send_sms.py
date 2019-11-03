afc64f860cde34389afa73f086a3723c597d8ef6
Divya Chowbey <divyachowbey@gmail.com>
2019-11-02 20:04:59 -0400

    Protecting twilio's credentials. need tokens from local .secrets file

--- send_sms.py
+++ send_sms.py
@@ -1,11 +1,12 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
+import os
 from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACa8b8499c4379b5b36d473cc857d8d6e3'
-auth_token = '210e881f87774813168e3150c46b5181'
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN'] 
 client = Client(account_sid, auth_token)
 
 message = client.messages \
